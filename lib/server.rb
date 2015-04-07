c1d176574a299573fe2a0f58cba118910d07274c
Joseph Knowles <joejknowles@gmail.com>
2015-04-07 17:29:31 +0100

    implemented login

--- lib/server.rb
+++ lib/server.rb
@@ -4,13 +4,51 @@ require 'data_mapper'
 env = ENV['RACK_ENV'] || 'development'
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-require 'link'
+require_relative 'link'
+require_relative 'tag'
+require_relative 'user'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
   get '/' do
-    "Hello World"
+    @links = Link.all
+    erb :index
+  end
+
+  post '/links' do
+    url = params["url"]
+    title = params["title"]
+    tags = params["tags"].split(' ').map do |tag|
+      Tag.first_or_create(text: tag)
+    end
+    Link.create(url: url, title: title, tags: tags)
+    redirect to('/')
+  end
+
+  get '/tags/:text' do
+    tag = Tag.first(text: params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                       password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
   end
 
   run! if app_file == $PROGRAM_NAME
